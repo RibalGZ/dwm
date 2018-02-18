8052ee8dbbc4199992ab39e997e9d04af9a4ba27
Rubén Santos <kojicomics@cocaine.ninja>
2018-02-18 21:37:37 +0100

    Basic underline support

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int taglinepx = 2;        /* height of tag underline */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Tamzen:size=10", "Siji:size=10", };
